57d64e03571383bdd34c35597d8fbeb600f0665b
Abnormally <darkling.pk@gmail.com>
2017-10-29 00:06:58 +0300

    Inial [#0]
    Just initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use App\Observers\UserObserver;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * Adding observer on class booting
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::observe(UserObserver::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Darkling
+ * Date: 28.10.2017
+ * Time: 23:22
+ */
+
+namespace App\Observers;
+
+class UserObserver
+{
+    public function created() {
+
+    }
+}
